d2d8637ab7bdb2015f4905c9f496bd715b8bb9aa
Shaurya Prashar <shaurya1269@gmail.com>
2026-04-11 00:05:59 +0530

    switched from sqlite3 to postgre

--- app/__init__.py
+++ app/__init__.py
@@ -1,34 +1,35 @@
 from flask import Flask
-from app.extensions import db
+import os
+import psycopg2
+from dotenv import load_dotenv
 
 
 def create_app():
     app = Flask(__name__)
 
     # config
-    import os
-    from dotenv import load_dotenv
     load_dotenv()
     app.secret_key = os.getenv('SECRET_KEY', 'fallback_secret_key')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
-        'DATABASE_URL', 'sqlite:///shop.db')
 
-    # db init
-    db.init_app(app)
-    
-    
     @app.route("/init-db")
     def init_db():
-        import app.models.user_model
-        import app.models.product_model
-        import app.models.shop_model
-        db.create_all()
-        return "Database intialized!!"
-    
+        schema_path = os.path.join(os.path.dirname(
+            __file__), '..', 'database', 'schema.sql')
+        db_path = os.path.join(os.path.dirname(__file__), '..', 'shop.db')
+        with open(schema_path, 'r') as f:
+            schema = f.read()
+        conn = psycopg2.connect(os.getenv("postgresql://shaurya:WO9M0uxXeskbGy3Lm8RmgbrPNMycN749@dpg-d74e8h450q8c73duv8ig-a.oregon-postgres.render.com/shopplatform123"))
+        conn.executescript(schema)
+        conn.commit()
+        conn.close()
+        return "Database initialized!!"
+
     # blueprint registration
     from app.routes.shop_routes import shop_bp
     from app.routes.auth_routes import auth_bp
+    from app.routes.order_routes import order_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
     app.register_blueprint(shop_bp)
+    app.register_blueprint(order_bp)
 
     return app
--- database/create_tables.py
+++ database/create_tables.py
@@ -1,11 +1,11 @@
-import sqlite3
+import psycopg2
 import os
 
 # Path to the database
 db_path = os.path.join(os.path.dirname(__file__), '..', 'shop.db')
 
 # Connect to SQLite database
-conn = sqlite3.connect(db_path)
+conn = psycopg2.connect(os.getenv("postgresql://shaurya:WO9M0uxXeskbGy3Lm8RmgbrPNMycN749@dpg-d74e8h450q8c73duv8ig-a.oregon-postgres.render.com/shopplatform123"))
 cur = conn.cursor()
 
 # Read and execute the schema
